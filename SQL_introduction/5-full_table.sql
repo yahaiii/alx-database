8da9188f32188949cedfd4215b75ce705b5fba13
Yahaya Aluke <52191233+yahaiii@users.noreply.github.com>
2023-08-15 02:58:06 +0100

    modify 5-full_table

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,2 +1,18 @@
--- shows full description of hbtn_0c_0.first_table;
-SHOW CREATE TABLE hbtn_0c_0.first_table;
+-- This command will print the full description of the table `first_table` from the database `hbtn_0c_0` in your MySQL server.
+
+-- Check if the table exists
+SHOW TABLES LIKE 'first_table';
+
+-- If the table exists, print the CREATE TABLE statement
+IF EXISTS (
+  SELECT *
+  FROM INFORMATION_SCHEMA.TABLES
+  WHERE TABLE_NAME = 'first_table'
+  AND TABLE_SCHEMA = 'hbtn_0c_0'
+)
+THEN
+  SHOW CREATE TABLE hbtn_0c_0.first_table;
+ELSE
+  -- The table does not exist, so print an error message
+  SELECT 'The table `first_table` does not exist.';
+END IF;
